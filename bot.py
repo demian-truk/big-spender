c639f172e2b31cf74cdb118972f666ef26f42917
Dima Teteruk <dimtruk93@gmail.com>
2023-03-04 21:25:02 +0300

    Restrict access to the chat with the bot to other users

--- bot.py
+++ bot.py
@@ -3,12 +3,16 @@ import os
 
 from aiogram import Bot, Dispatcher, executor
 
+from middlewares import AccessMiddleware
+
 logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(AccessMiddleware(int(ACCESS_ID)))
 
 
 if __name__ == "__main__":
