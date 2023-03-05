4322d5483009c4ce4125010c50ecb6baf131abc3
Dima Teteruk <dimtruk93@gmail.com>
2023-03-05 20:58:47 +0300

    Add a welcome function for telegram bot

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import logging
 import os
 
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher, executor, types
 
+from keyboards import main_menu_keyboard
 from middlewares import AccessMiddleware
 
 logging.basicConfig(level=logging.INFO)
@@ -13,6 +14,15 @@ ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(AccessMiddleware(int(ACCESS_ID)))
+
+
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    """Send a welcome message"""
+    await message.answer(
+        "Привет 👋\nСобираешься опять потратить наши денежки? 😄",
+        reply_markup=main_menu_keyboard,
+    )
 
 
 if __name__ == "__main__":
