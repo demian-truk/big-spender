6c7e1a2355d930e844d6d4542479cb5e581e87d3
Dima Teteruk <dimtruk93@gmail.com>
2023-03-06 12:24:57 +0300

    Add functions for getting/adding/updating income to the telegram bot

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 from aiogram import Bot, Dispatcher, executor, types
 
 from keyboards import main_menu_keyboard
+from main import get_income, update_or_create_income
 from middlewares import AccessMiddleware
 
 logging.basicConfig(level=logging.INFO)
@@ -23,6 +24,33 @@ async def send_welcome(message: types.Message):
         "Привет 👋\nСобираешься опять потратить наши денежки? 😄",
         reply_markup=main_menu_keyboard,
     )
+
+
+@dp.message_handler(lambda message: message.text.startswith("Доход"))
+async def add_income(message: types.Message):
+    """Create or update income"""
+    user_message = message.text.replace("Доход", "").replace(" ", "")
+    update_or_create_income(float(user_message))
+    await message.answer("Баланс успешно изменен ✅")
+
+
+@dp.message_handler()
+async def main_menu(message: types.Message):
+    if message.text == "💵 Доходы":
+        income = get_income()
+        if not income:
+            await message.answer(
+                "В твоих карманах совсем нет эдди 😢"
+                + "\n\nДля добавления дохода введи сообщение в формате:\nДоход 125.30"
+            )
+        else:
+            await message.answer(
+                "Будь спокоен, ты еще не банкрот 😄🥳"
+                + "\n\nОстаток денежных средств:\n"
+                + str(income.amount)
+                + " BYN"
+                + "\n\nДля добавления дохода введи сообщение в формате:\nДоход 125.30"
+            )
 
 
 if __name__ == "__main__":
